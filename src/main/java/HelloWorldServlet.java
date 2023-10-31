3d7954c9924b7df556071d729218cbaa54c6350c
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-10-31 11:40:12 -0500

    completed hello world bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,19 +5,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("name");
+
+        if(name == null) {
+            name = "World";
+        }
+
         try {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
-            out.println("<h1>Hello, World!</h1>");
+            out.println("<h1>Hello, " + name + "!</h1>");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
 }
