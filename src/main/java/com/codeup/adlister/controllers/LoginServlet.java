304380a9e221fd6b54359b4440eae47b01ca85fb
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-11-08 11:57:59 -0600

    added comments in register and login servlets

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -22,23 +22,26 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //stores the plaintext username and password the user typed into the form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-
+        //see if the user exists in the database and return a User obj
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
-        boolean passwordsMatch = BCrypt.checkpw(password, user.getPassword());
-        System.out.println(passwordsMatch);
-
-
+        //if the user returned is Null, then the user did not exist in the DB
+        //maybe they typed in their login info incorrect? send them back to the login screen
         if (user == null) {
             response.sendRedirect("/login");
             return;
         }
 
+        //gets the hash from the DB that belongs to the user and checks to see if the hash belongs to the password the user typed in
+        //returns True or False
+        boolean passwordsMatch = BCrypt.checkpw(password, user.getPassword());
 
 
+        //if it's true that the stored hash belongs to the password the user typed in, then allow them to login and take them to their profile
         if (passwordsMatch) {
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -18,16 +18,18 @@ public class RegisterServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //stores the plaintext info the user typed into the form
         String username = request.getParameter("username");
         String email = request.getParameter("email");
-
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
+        //if the plain text password and password confirmation match, then hash the password and store the hash in a variable
         String hashedPassword = new String();
         if (password.equals(passwordConfirmation)) {
            hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
         } else {
+            //if the plaintext password and passwordConfirmation DO NOT match, redirect them to the registration form
             response.sendRedirect("/register");
         }
 
@@ -42,7 +44,7 @@ public class RegisterServlet extends HttpServlet {
             return;
         }
 
-        // create and save a new user
+        // create and save a new user, store the hashedPassword in the DB
         User user = new User(username, email, hashedPassword);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
